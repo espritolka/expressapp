1eb25db43619da2c9b14333e44ce430360b7dd73
Iugai Olga <espritolka@gmail.com>
2019-03-02 14:18:41 +0300

    test params

--- app.js
+++ app.js
@@ -23,13 +23,38 @@ app.use("/home/foo/bar/404", function (request, response) {
     response.status(404).send(`Ресурс не найден`);
    });
 app.use("/home/foo/bar",function (request, response) {
-      response.redirect(".")
+      response.redirect(301, ".")
 });   //переадресация на уровень выше
 app.use("/index/lol",function (request, response) {
     response.redirect("..")
   }); //переадресация на 2 уровня выше
   app.get("/home/foo", function (request, response) {
     response.send("Hello foo page");
+});
+app.use("/about", function(request, response){
+      
+    let id = request.query.id;
+    let userName = request.query.name;
+    response.send("<h1>Информация</h1><p>id=" + id +"</p><p>name=" + userName + "</p>");
+});
+app.use("/users", function(request, response){
+      
+    console.log(request.query);
+    let names = request.query.name;
+    let responseText = "<ul>";
+    for(let i=0; i < names.length; i++){
+        responseText += "<li>" + names[i] + "</li>";
+    }
+    responseText += "</ul>";
+    response.send(responseText);
+});
+app.use("/globalusers", function(request, response){
+      
+    console.log(request.query);
+    let id = request.query.user.id;
+    let name = request.query.user.name;
+     
+    response.send("<h3>id:" + id + "<br>name: " + name +"</h3>");
 });
   app.get("/home", function (request, response) {
     response.send("Hello home page");
