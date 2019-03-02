539643d9552fbdaabb3a4053958ccc26ed780c6d
Iugai Olga <espritolka@gmail.com>
2019-03-02 17:16:46 +0300

    add producs routs

--- app.js
+++ app.js
@@ -32,12 +32,12 @@ app.use("/index/lol",function (request, response) {
   app.get("/home/foo", function (request, response) {
     response.send("Hello foo page");
 });
-app.use("/about", function(request, response){
+// app.use("/about", function(request, response){
       
-    let id = request.query.id;
-    let userName = request.query.name;
-    response.send("<h1>Информация</h1><p>id=" + id +"</p><p>name=" + userName + "</p>");
-});
+//     let id = request.query.id;
+//     let userName = request.query.name;
+//     response.send("<h1>Информация</h1><p>id=" + id +"</p><p>name=" + userName + "</p>");
+// });
 app.use("/users", function(request, response){
       
     console.log(request.query);
@@ -74,16 +74,30 @@ app.post("/register", urlencodedParser, function (request, response) {
     response.send(`${request.body.userName} - ${request.body.userAge}`);
 });
  
-app.get("/products/:productId", function (request, response) {
-    response.send("productId: " + request.params["productId"])
+// app.get("/products/:productId", function (request, response) {
+//     response.send("productId: " + request.params["productId"])
+//   });
+// app.get("/categories/:categoryId/products/:productId", function (request, response) {
+//     let catId = request.params["categoryId"];
+//     let prodId = request.params["productId"];
+//     response.send(`Категория: ${catId}  Товар: ${prodId}`);
+// });  
+app.use("/about", function (request, response) {
+    response.send("О сайте");
   });
-app.get("/categories/:categoryId/products/:productId", function (request, response) {
-    let catId = request.params["categoryId"];
-    let prodId = request.params["productId"];
-    response.send(`Категория: ${catId}  Товар: ${prodId}`);
-});  
+   
+  app.use("/products/create",function (request, response) {
+    response.send("Добавление товара");
+  });
+  app.use("/products/:id",function (request, response) {
+    response.send(`Товар ${request.params.id}`);
+  });
+  app.use("/products/",function (request, response) {
+    response.send("Список товаров");
+  });
+
 app.get("/", function (request, response) {
-    response.send("Hello main page");
+    response.send("<h1>Hello main page</h1> <small>I'm test <b>app</b></small>");
 });
 
 app.listen(3000);
