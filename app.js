ead2d52eec8f464def42ef4993acd46e0581f110
Iugai Olga <espritolka@gmail.com>
2019-03-03 11:38:04 +0300

    add array to hbs

--- app.js
+++ app.js
@@ -73,8 +73,13 @@ app.set("view engine", "hbs");
  
 app.use("/contact", function(request, response){
      
-    response.render("contact.hbs");
-}); 
+    response.render("contact.hbs", {
+        title: "Мои контакты",
+        emailsVisible: true,
+        emails: ["gavgav@mycorp.com", "mioaw@mycorp.com"],
+        phone: "+1234567890"
+    });
+});
 app.get("/home", function(request, response){
       
     response.sendFile(__dirname + "/index.html");
