71fbba03126066376238bbd5a5df3b144ac491a8
Iugai Olga <espritolka@gmail.com>
2019-03-03 11:20:58 +0300

    test json resp

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const fs = require("fs");
-const bodyParser = require("body-parser");
+const bodyParser = require("body-parser"); // определяем Router
+const productRouter = express.Router();
+const jsonParser = express.json(); // создаем парсер для данных в формате json
 const app = express();
 const urlencodedParser = bodyParser.urlencoded({extended: false}); //парсер для получения отправленных данных 
 app.use(function (request, response, next) {
@@ -23,21 +25,7 @@ app.use("/home/foo/bar/404", function (request, response) {
     //   response.sendStatus(404)
     response.status(404).send(`Ресурс не найден`);
    });
-app.use("/home/foo/bar",function (request, response) {
-      response.redirect(301, ".")
-});   //переадресация на уровень выше
-app.use("/index/lol",function (request, response) {
-    response.redirect("..")
-  }); //переадресация на 2 уровня выше
-  app.get("/home/foo", function (request, response) {
-    response.send("Hello foo page");
-});
-// app.use("/about", function(request, response){
-      
-//     let id = request.query.id;
-//     let userName = request.query.name;
-//     response.send("<h1>Информация</h1><p>id=" + id +"</p><p>name=" + userName + "</p>");
-// });
+
 app.use("/users", function(request, response){
       
     console.log(request.query);
@@ -49,22 +37,7 @@ app.use("/users", function(request, response){
     responseText += "</ul>";
     response.send(responseText);
 });
-app.use("/globalusers", function(request, response){
-      
-    console.log(request.query);
-    let id = request.query.user.id;
-    let name = request.query.user.name;
-     
-    response.send("<h3>id:" + id + "<br>name: " + name +"</h3>");
-});
-  app.get("/home", function (request, response) {
-    response.send("Hello home page");
-});
-app.use("/index", function (request, response) {
-    response.sendFile(__dirname + "/index.html");
-});
 
- 
 app.get("/register(.html)?", urlencodedParser, function (request, response) {
     response.sendFile(__dirname + "/register.html");
 });
@@ -73,31 +46,35 @@ app.post("/register", urlencodedParser, function (request, response) {
     console.log(request.body);
     response.send(`${request.body.userName} - ${request.body.userAge}`);
 });
- 
-// app.get("/products/:productId", function (request, response) {
-//     response.send("productId: " + request.params["productId"])
-//   });
-// app.get("/categories/:categoryId/products/:productId", function (request, response) {
-//     let catId = request.params["categoryId"];
-//     let prodId = request.params["productId"];
-//     response.send(`Категория: ${catId}  Товар: ${prodId}`);
-// });  
+  
 app.use("/about", function (request, response) {
     response.send("О сайте");
   });
-   
-  app.use("/products/create",function (request, response) {
+ // определяем маршруты и их обработчики внутри роутера
+productRouter.use("/create", function(request, response){
     response.send("Добавление товара");
   });
-  app.use("/products/:id",function (request, response) {
+  productRouter.use("/:id", function(request, response){
     response.send(`Товар ${request.params.id}`);
   });
-  app.use("/products/",function (request, response) {
+  productRouter.use("/", function(request, response){
     response.send("Список товаров");
   });
-
-app.get("/", function (request, response) {
-    response.send("<h1>Hello main page</h1> <small>I'm test <b>app</b></small>");
+  // сопотавляем роутер с конечной точкой "/products"
+app.use("/products", productRouter);
+app.post("/user", jsonParser, function (request, response) {
+    console.log(request.body);
+    if(!request.body) return response.sendStatus(400);
+    console.log(request.body);
+    response.json(`${request.body.userName} - ${request.body.userAge}`);
 });
+  
+app.get("/", function(request, response){
+      
+    response.sendFile(__dirname + "/index.html");
+});
+// app.get("/", function (request, response) {
+//     response.send("<h1>Hello main page</h1> <small>I'm test <b>app</b></small>");
+// });
 
 app.listen(3000);
